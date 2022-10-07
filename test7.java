b99869083a2f9de0b8bbfecdecaab32ad6e6879c
Do0727do <114710202+Do0727do@users.noreply.github.com>
2022-10-07 10:28:32 +0800

    Add files via upload

---
+++ test7.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class test7{
+	public static void main (String[] args) {
+		int grade;
+		Scanner input = new Scanner(System.in);
+			System.out.println("Enter first integer:");
+			grade = input.nextInt();
+			
+			if(grade > 100 || grade < 0) 
+			System.out.println('x');
+			else{
+			switch(grade/10){
+			case 10:
+			case 9:
+			System.out.println('A');
+			break;
+			case 8:
+			System.out.println('B');
+			break;
+			case 7:
+			System.out.println('C');
+			break;
+			case 6:
+			System.out.println('D');
+			break;
+			case 5:
+			case 4:
+			case 3:
+			case 2:
+			case 1:
+			case 0:
+			System.out.println('E');
+			default:
+			break;
+				}
+			}
+		}
+	}
+		
+
+
+
+				
