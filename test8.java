1c2f41a16fb22fbde4d6f3bd4e28a86e49c88e87
Do0727do <114710202+Do0727do@users.noreply.github.com>
2022-10-07 11:41:49 +0800

    Add files via upload

---
+++ test8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class test8{
+	public static void main (String[] args) {
+		int grade =0;
+		String space ="";
+		 
+		while (grade<10) {
+
+			System.out.printf("%d %n%s ",grade,space);
+			grade = grade+1;
+			space = space + " ";
+			
+		}
+	}
+}
+
+
+
+				
