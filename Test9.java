0dfdfb557a1f74d77f15da62723685755d0b8a01
Do0727do <114710202+Do0727do@users.noreply.github.com>
2022-10-14 11:45:04 +0800

    Add files via upload

---
+++ Test9.java
@@ -0,0 +1,19 @@
+public class Test9{
+    public static void main(String[] args){
+        int row =0;
+		String star = "*";
+		
+		while (row <5) {
+			int b = 4-row;
+			while (b >0){
+				 System.out.print(" ");
+				 b=b-1;
+				 
+			}
+			System.out.println(star);
+			star=star + "**";
+			
+			 row++;
+		}
+	}
+}
